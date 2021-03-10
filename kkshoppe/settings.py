7654ced181689cdcbcc11e78309c1f8ee4ee6063
Marianne <hernaez.marianne@gmail.com>
2021-03-10 14:56:12 +0800

    change database

--- kkshoppe/settings.py
+++ kkshoppe/settings.py
@@ -75,12 +75,15 @@ WSGI_APPLICATION = 'kkshoppe.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
+if 'DATABASE_URL' in os.environ:
+    import dj_database_url
+    DATABASES = {'default': dj_database_url.config()}
 
 
 # Password validation
