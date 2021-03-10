a38ebb162a4e4d2bfeb021c091b253215e38d37f
Marianne <hernaez.marianne@gmail.com>
2021-03-10 15:09:18 +0800

    add dbsqlite3 to repo

--- kkshoppe/settings.py
+++ kkshoppe/settings.py
@@ -12,7 +12,6 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 
 import os
 import dj_database_url 
-import django_heroku
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -134,6 +133,4 @@ STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 prod_db  =  dj_database_url.config(conn_max_age=500)
 DATABASES['default'].update(prod_db)
-
-django_heroku.settings(locals())
 
